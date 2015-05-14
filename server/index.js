152ebb95d04c68f80a634308609b7a4c0a8e1654
capellaresumo <capellaresumo@gmail.com>
2015-05-14 08:55:33 -0300

    Atualizando o Servidor

--- server/index.js
+++ server/index.js
@@ -7,13 +7,14 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var config = require('config');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = config.get("appPort");        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
