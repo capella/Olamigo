696d5150a43f79cc9be5278a1ff90f45b278a362
capellaresumo <capellaresumo@gmail.com>
2015-05-14 16:32:21 -0300

    Testando

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ var router = express.Router();              // get an instance of the express Ro
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api  001!'+config.MYSQL.host });   
+    res.json({ message: 'hooray! welcome to our api  003!'+config.MYSQL.host });   
 });
 
 // more routes for our API will happen here
