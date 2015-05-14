046d0d39e649ffaa00028f09c49e156694863565
capellaresumo <capellaresumo@gmail.com>
2015-05-14 17:31:26 -0300

    Arrumando Servidor

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ var port = config.porta_aplicativo;        // set our port
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
-var gostos = express.Router();              // get an instance of the express Router
+//var gostos = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 /*router.get('/', function(req, res) {
@@ -41,6 +41,14 @@ gostos.post('/', function(req, res, next) {
 // all of our routes will be prefixed with /api
 //app.use('/api', router);
 app.use('/gostos', gostos);
+
+
+//ERROS ------
+
+app.use(function(err, req, res, next) {
+	res.status(500);
+	res.send(err.stack);
+});
 
 // START THE SERVER
 // =============================================================================
